2b7dc26ad79ef037b909b523b3ef329fcbf62325
Denis Torre <torred23@li03c04.chimera.hpc.mssm.edu>
2020-07-09 15:24:37 -0400

    Added aggregate_counts

--- pipeline/scripts/alignment.R
+++ pipeline/scripts/alignment.R
@@ -13,13 +13,44 @@
 
 #######################################################
 #######################################################
-########## S1. 
+########## S1. Aggregate counts
 #######################################################
 #######################################################
 
 #############################################
-########## 1. 
+########## 1. Function to aggregate counts
 #############################################
+# Aggregates counts from STAR, kallisto or salmon.
+
+aggregate_counts <- function(infiles, method, txOut=TRUE, genome='hg38', name_func=function(x) { x_split <- strsplit(x, '/')[[1]]; x_name <- x_split[length(x_split)-1]; return(x_name); }, star_col=2) {
+
+    # Add names
+    names(infiles) <- sapply(infiles, name_func)
+
+    # STAR
+    if (method == 'star') {
+         
+        # Read and concatenate
+        concatenated_dataframe <- do.call('rbind', sapply(names(infiles), function(x) {df <- as.data.frame(data.table::fread(infiles[x])); df <- df[grepl('ENSG*', df$V1),]; colnames(df)[1] <- 'ensembl_gene_id'; colnames(df)[star_col] <- 'counts'; df$sample <- x; df}, simplify=FALSE))
+        
+        # Cast
+        result <- reshape2::dcast(ensembl_gene_id ~ sample, data = concatenated_dataframe, value.var='counts', fun.aggregate=sum)
+               
+    # Kallisto and Salmon
+    } else if (method %in% c('kallisto', 'salmon')) {
+      
+        # Get tx2g
+        tx2g <- data.table::fread(Sys.glob(glue::glue('/sc/hydra/projects/GuccioneLab/genome-indices/{genome}/ensembl/*-biomart.txt')))
+
+        # Tximport
+        result <- tximport::tximport(infiles, type=method, txOut=txOut, tx2g=tx2g[,c('Transcript stable ID version', 'Gene stable ID')])
+
+    }
+    
+    # Return
+    return(result)
+        
+}
 
 
 #######################################################
